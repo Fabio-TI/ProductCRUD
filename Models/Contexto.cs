ffb3eef21e8d9631f3f651ac923dd12cb6bf310e
Fábio Lima <fabiojr.lim@hotmail.com>
2024-06-26 12:27:59 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace ProductCRUD.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options)
+        {
+
+        }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
